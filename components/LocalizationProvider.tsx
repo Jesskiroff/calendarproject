2dd65c174be6defbd81c8de37a00ff21cf23fed3
Jessica Kiroff <jesskiroff@gmail.com>
2024-04-19 17:34:46 -0400

    add components folder

--- components/LocalizationProvider.tsx
+++ components/LocalizationProvider.tsx
@@ -1,14 +1,16 @@
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
-function LocalizationProviderWrapper({ children }:  {
-children: React.ReactNode }) {
+function LocalizationProviderWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       {children}
     </LocalizationProvider>
-  )
+  );
 }
-
 
 export default LocalizationProviderWrapper;
